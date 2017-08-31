8e0d3fbf274014c39b68d70bb7b103ab6f271550
ira-kabalyuk <jokky@bigmir.net>
2017-08-31 14:29:40 +0300

    tab block dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -137,46 +137,47 @@ jQuery(document).ready(function() {
 
 	$(".search-window__content-block_text").shorten({
 		"showChars" : 80,		
-});
+	});
 
+	 // tabbed content
+    // http://www.entheosweb.com/tutorials/css/tabs.asp
+    $(".tab_content").hide();
+    $(".tab_content:first").show();
 
-  //checkbox block
+  /* if in tab mode */
+    $("ul.tabs li").click(function() {
+		
+      $(".tab_content").hide();
+      var activeTab = $(this).attr("rel"); 
+      $("#"+activeTab).fadeIn();		
+		
+      $("ul.tabs li").removeClass("active");
+      $(this).addClass("active");
 
-   $(".checkbox-block").click(function( e ) {        
-       $(".checkbox-block").removeClass("block-active")
-        $(this).addClass("block-active");
-       
-    });     
-
-  //end checkbox block 
-
- 
-
-  //catalog-menu
-
-   $(".catalog-menu-list li").click(function( e ) {        
-       $(".catalog-menu-list li").removeClass("menu-active")
-        $(this).addClass("menu-active");
-       
+	  $(".tab_drawer_heading").removeClass("d_active");
+	  $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+	  
     });
-
-
-
-    //paging
-
-     $(".paging-list a").click(function( e ) {        
-       $(".paging-list a").removeClass("page-active")
-        $(this).addClass("page-active");
-       
-    });  
-
-    //end paging 
-
-   /* objectFit.polyfill({
-		selector: 'img', // this can be any CSS selector
-		fittype: 'contain', // either contain, cover, fill or none
-		disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
-	});  */
+	/* if in drawer mode */
+	$(".tab_drawer_heading").click(function() {
+      
+      $(".tab_content").hide();
+      var d_activeTab = $(this).attr("rel"); 
+      $("#"+d_activeTab).fadeIn();
+	  
+	  $(".tab_drawer_heading").removeClass("d_active");
+      $(this).addClass("d_active");
+	  
+	  $("ul.tabs li").removeClass("active");
+	  $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+    });
+	
+	
+	/* Extra class "tab_last" 
+	   to add border to right side
+	   of last tab */
+	$('ul.tabs li').last().addClass("tab_last");
+	
 
 
 });
