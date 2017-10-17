62ba69c5de026d2a7115bc6135ca92f90c7462cc
ira-kabalyuk <jokky@bigmir.net>
2017-10-17 18:19:55 +0300

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -257,7 +257,24 @@ $('.btn-prev').on('click',function(){ $('#slick').slick('slickPrev'); });*/
 
 });
 
+	var userAgent, ieReg, ie, ieEdge, ieE;
+	userAgent = window.navigator.userAgent;
+	ieReg = /msie|Trident.*rv[ :]*11\./gi;
+	ieEdge = /Edge/i;
+	ie = ieReg.test(userAgent);
+	ieE= ieEdge.test(userAgent);
 
+	if(ie || ieE) {
+		$(".img-wrap").each(function () {
+			var $container = $(this),
+					imgUrl = $container.find("img").prop("src");
+			if (imgUrl) {
+				$container.css("backgroundImage", 'url(' + imgUrl + ')').addClass("custom-object-fit");
+			}
+		});
+	}
+
+	
 	
 
 
